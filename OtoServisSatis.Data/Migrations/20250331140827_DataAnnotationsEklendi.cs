1596f37aeee2ae1bab21f6eb5b853ef2b7be88c0
mustafalmazz <mustafagfb19077091@gmail.com>
2025-03-31 18:06:55 +0300

    DataAnottations düzenlemeleri
    Entity classlarıında DataAnnotations düzenlemeleri yapıldı .Kullanıcı düzenleme ve silme sayfaları eklendi

---
+++ OtoServisSatis.Data/Migrations/20250331140827_DataAnnotationsEklendi.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OtoServisSatis.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataAnnotationsEklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefon",
+                table: "Kullanicilar",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KullaniciAdi",
+                table: "Kullanicilar",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EklenmeTarihi",
+                value: new DateTime(2025, 3, 31, 17, 8, 27, 192, DateTimeKind.Local).AddTicks(9733));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefon",
+                table: "Kullanicilar",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KullaniciAdi",
+                table: "Kullanicilar",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EklenmeTarihi",
+                value: new DateTime(2025, 3, 23, 0, 31, 24, 373, DateTimeKind.Local).AddTicks(5666));
+        }
+    }
+}
--- OtoServisSatis.Entities/Marka.cs
+++ OtoServisSatis.Entities/Marka.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace OtoServisSatis.Entities
 {
     public class Marka : IEntity
     {
         public int Id { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Adı"), Required(ErrorMessage = "{0} Boş Bırakılamaz")]
         public string Adi { get; set; }
-
 
     }
 }
